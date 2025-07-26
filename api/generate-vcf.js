7a71521612c96f79c06596cae60eb75608adfc11
Marchech <emprerorsukuna@gmail.com>
2025-07-25 23:31:00 -0400

    Create generate-vcf.js

---
+++ api/generate-vcf.js
@@ -0,0 +1,18 @@
+export default function handler(req, res) {
+  if (req.method === "POST") {
+    const { name, email, phone } = req.body;
+    const vcf = `
+BEGIN:VCARD
+VERSION:3.0
+FN:${name}
+TEL:${phone}
+EMAIL:${email}
+END:VCARD
+`.trim();
+    res.setHeader('Content-Type', 'text/vcard');
+    res.setHeader('Content-Disposition', `attachment; filename=${name}.vcf`);
+    res.status(200).send(vcf);
+  } else {
+    res.status(405).send("Method Not Allowed");
+  }
+}
